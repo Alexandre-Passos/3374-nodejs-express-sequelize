b6f1cd32465cd9a6f0552fc5923d8e6c12e7243f
Alexandre-Passos <alexandre15passos@gmail.com>
2025-07-06 14:48:46 -0300

    Correções de atualização de dados concluida

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const pessoas = require('./pessoasRoute.js');
 
-
 module.exports = app => {
-    app.use(express.json(),
+    app.use(
+        express.json(),
         pessoas,
     );
 };
